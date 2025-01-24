a450112cba61c7887be46cc3cb816d64e5499d77
prabalab <40490211+prabalab@users.noreply.github.com>
2025-01-24 20:25:36 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/userRoutes'); // Import the router
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+// Use the router
+app.use('/users', userRoutes);
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
